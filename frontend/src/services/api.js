92648e895c16d3d31ec3eb3afcb813b6ee5e2dcf
shivamjee1 <shivam8621116@gmail.com>
2026-09-22 18:13:13 +0530

    commit 4th frontend setup

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://arms-backend-q0ly.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
